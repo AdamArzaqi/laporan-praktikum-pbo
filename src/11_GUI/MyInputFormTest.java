34e63be0a76fb99ecc6143a3c7e8c2b4d46e1998
AdamArzaqi <54811450+AdamArzaqi@users.noreply.github.com>
2019-11-11 23:39:06 +0700

    Add files via upload

---
+++ src/11_GUI/MyInputFormTest.java
@@ -0,0 +1,13 @@
+package HelloGui_1841720075Adam;
+
+import javax.swing.JFrame;
+
+public class MyInputFormTest {
+
+    public static void main(String[] args) {
+        JFrame frame=new MyInputForm();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }   
+}
